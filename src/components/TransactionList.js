ba768a2912b70d9332c8bb2517b7bcc10b009f1e
guruhedipurwanto <guruhedipurwanto@gmail.com>
2020-08-06 15:54:16 +0800

    TransactionList now render the provided context

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -1,16 +1,23 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { GlobalContext } from '../context/GlobalState';
 
 export default function TransactionList() {
+  const { transactions } = useContext(GlobalContext);
+
   return (
     <React.Fragment>
       <h3>History</h3>
       <ul id="list" className="list">
-        <li className="minus">
-          Cash <span>-$400</span>
-          <button className="waves-effect waves-light btn blue lighten-1">
-            x
-          </button>
-        </li>
+        {transactions.map((transaction) => {
+          return (
+            <li key={transaction.id} className="minus">
+              {transaction.text} <span>-$400</span>
+              <button className="waves-effect waves-light btn blue lighten-1">
+                x
+              </button>
+            </li>
+          );
+        })}
       </ul>
     </React.Fragment>
   );
