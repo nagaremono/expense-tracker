0560299804329695b34b326b357380ba17fc30ab
guruhedipurwanto <guruhedipurwanto@gmail.com>
2020-08-06 16:15:52 +0800

    Balance component now render dynamically following the provided context

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { GlobalContext } from '../context/GlobalState';
 
 export default function Balance() {
+  const { transactions } = useContext(GlobalContext);
+
+  const amounts = transactions.map((transaction) => transaction.amount);
+
+  const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
+
   return (
     <div className="balance">
       <h3>Your Balance</h3>
-      <h4>$0.00</h4>
+      <h4>${total}</h4>
     </div>
   );
 }
